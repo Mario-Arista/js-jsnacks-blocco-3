690d1db2245d90d2b4bb9a94a0215783b71f170b
mario.arista <mario.arista.dev@gmail.com>
2024-02-12 17:46:25 +0100

    almost done

--- snack-5/script.js
+++ snack-5/script.js
@@ -5,15 +5,32 @@ la stessa stringa ma con le iniziali
 delle parole tutte in maiuscolo
 */
 
-const phrase = `uomo che usciva la gente`;
+const phrase = "uomo che usciva la gente";
 
-  function capitalWords(string) {
+function capitalWords(string) {
 
-    // Divido la stringa in un array di parole
-    let words = string.split(' ');
-    console.log(words);
+  // Divido la stringa in un array di parole
+  let words = string.split(" ");
+  console.log(words);
+
+  // Creo array per memorizzare le parole con la prima lettera maiuscola
+  let FirstLetterCapitalWords = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let letters = words[i].split("");
+
+    letters[0] = letters[0].toUpperCase();
+
+    let capitallizedword = letters.join("");
+
+    FirstLetterCapitalWords.push(capitallizedword);
+
+  }
+
+  console.log(FirstLetterCapitalWords);
+
+  return FirstLetterCapitalWords;
 
 }
 
-  
-  const output = document.getElementById("output").innerText = capitalWords(phrase);
+const output = document.getElementById("output").innerText = capitalWords(phrase);
