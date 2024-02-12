fc7e9bb5ce2594729776a483493bbc5f5ae726e1
mario.arista <mario.arista.dev@gmail.com>
2024-02-12 14:40:44 +0100

    snack 3 with console log

---
+++ snack-3/script.js
@@ -0,0 +1,26 @@
+/*
+Scrivi una funzione che accetti 
+tre argomenti:
+un array e due numeri 
+(“a” più piccolo di “b” e “b” grande 
+al massimo quanto il numero di 
+elementi dell’array).
+*/
+
+// Dichiaro array come esempio
+const arrayNumeri = [1, 2, 3, 4, 5, 6];
+
+function cuttedArray(array, min, max) {
+
+    // dichiaro array unione
+    const arrayNuovo = [];
+
+    for (let i = min; i < max; i++) {
+        arrayNuovo.push(array[i]);
+    }
+
+    return arrayNuovo;
+    
+}
+
+console.log(cuttedArray(arrayNumeri, 0, 3));
